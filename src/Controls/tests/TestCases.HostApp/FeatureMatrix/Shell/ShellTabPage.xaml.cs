6f8bd8786432ad9328fbf8d1f155e52d4bc6932b
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2025-12-15 15:56:48 +0530

    TabBar issues

---
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/Shell/ShellTabPage.xaml.cs
@@ -0,0 +1,11 @@
+ namespace Maui.Controls.Sample;
+ using System;
+ using Microsoft.Maui.Controls;
+
+public partial class ShellTabPage : Shell
+{
+	public ShellTabPage()
+	{
+		InitializeComponent();
+	}
+}
